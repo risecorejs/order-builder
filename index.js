0a2521bbccaa2e64c9c71215696752b5405d96de
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2022-04-07 10:10:44 +0600

    add options{descNullsLast}

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
  * @param queryOrder {Array}
  * @param aliases {Object}
  * @param defaultOrder {Array}
+ * @param options {{
+ *   descNullsLast: boolean?
+ * }?}
  * @return {Array}
  */
-module.exports = (queryOrder, aliases, defaultOrder) => {
+module.exports = (queryOrder, aliases, defaultOrder, options) => {
   defaultOrder ||= []
 
   if (queryOrder) {
@@ -14,7 +17,11 @@ module.exports = (queryOrder, aliases, defaultOrder) => {
     if (['ASC', 'DESC'].includes(orderBy)) {
       for (const [_alias, order] of Object.entries(aliases)) {
         if (alias === _alias) {
-          order.push(orderBy)
+          if (orderBy === 'DESC' && options.descNullsLast) {
+            order.push(orderBy + ' NULLS LAST')
+          } else {
+            order.push(orderBy)
+          }
 
           return [order, ...defaultOrder]
         }
--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const orderBuilder = require('./index')
 
 module.exports = () => (req, res, next) => {
-  req.orderBuilder = (aliases, defaultOrder) => orderBuilder(req.query.order, aliases, defaultOrder)
+  req.orderBuilder = (aliases, defaultOrder, options) => orderBuilder(req.query.order, aliases, defaultOrder, options)
 
   next()
 }
